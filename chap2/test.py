34418f0fbeb71c03c5b382385f44e80949ddfb16
moonkyeonghan <mkh2573@gmail.com>
2023-09-20 02:55:14 +0000

    python

--- chap2/test.py
+++ chap2/test.py
@@ -1,1 +1,1 @@
-print("dddd")
+print("Game over")
---
+++ chap2/test1.py
@@ -0,0 +1,10 @@
+print("                             Trust Fund Buddy")
+print("Total your monthl spending so that your trust fund doesn't run out")
+print("<and you're forced to get a real job>.")
+print("")
+print("please enter the requested, monthly costs. Since yor're rich, ignore pennies and use only dollar amounts")
+print("")
+print("")
+
+car=input("Lamborghini Tune-Ups")
+car=
