ca0fbce47c76e682d61cc31c050626948c8d9397
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-10-10 17:01:18 +0900

    Update prob3.py

--- chap4/prob3.py
+++ chap4/prob3.py
@@ -1,1 +1,18 @@
+import random as rd
+print(" \tWelcome to 'Guess My Number'!\n")
 
+print("I'm thinking of a number between 1 and 100")
+print("Try to Guess it in as few attempts as possible\n")
+guess = 1
+stack=0;
+secretguess = rd.randrange(1,100)
+while guess != secretguess:
+  guess = int(input("take a guess:"))
+  if guess > secretguess:
+      print("Lower.....")
+  elif guess < secretguess:
+      print("upper")
+  elif guess == secretguess:
+      print("you guess it! the number was",secretguess)
+  stack += 1
+print("And it only took you",stack,"tries!")
