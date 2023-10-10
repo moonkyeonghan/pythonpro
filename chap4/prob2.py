18ca162cd230426be6b309d161371d7919aeb479
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-10-10 16:21:14 +0900

    Update prob2.py

--- chap4/prob2.py
+++ chap4/prob2.py
@@ -1,15 +1,35 @@
-import random as r:qw
+import random as rd
 
 
-herohp = rd.random(50,100)
-monsterhp = rd.random(50,100)
-print("hero HP :", herohp "monster HP :",monsterhp)
-win = print("succes")
-lose = print("fail")
-while herohp >= 0 or monsterhp >= 0
-  heroatt = rd.random(-10,20)
-  monsteratt = rd.random(-10,20)
+herohp = rd.randrange(50,100)
+monsterhp = rd.randrange(50,100)
+stack = 0;
+print("hero HP :", herohp ,"monster HP :",monsterhp)
+win = ("succes")
+lose = ("fail")
+while herohp >= 0 and monsterhp >= 0:
+  heroatt = rd.randrange(-10,20)
+  monsteratt = rd.randrange(-10,20)
   if heroatt > 0:
-    if monsteratt > 0
-       print("hero(HP:)", herohp ", attack:", heroatt ")", heroword," <-> monster(HP:    )"monsterhp,", attack:",monsteratt,")",monste)
-   heroword = print("
+    if monsteratt > 0:
+      herohp = herohp - monsteratt
+      monsterhp = monsterhp - heroatt
+      print("hero(HP:", herohp ,", attack:", heroatt ,")", win," <-> monster(HP:",monsterhp,"), attack:",monsteratt,")",win)
+    elif monsteratt < 0:
+      monsterhp = monsterhp - heroatt
+      print("hero(HP:", herohp ,", attack:", heroatt ,")", win," <-> monster(HP:",monsterhp,"), attack:",monsteratt,")",lose)
+  
+  if heroatt < 0:
+    if monsteratt > 0:
+      herohp = herohp - monsteratt
+      print("hero(HP:", herohp ,", attack:", heroatt ,")", lose," <-> monster(HP:",monsterhp,"), attack:",monsteratt,")",win)
+    else:
+      print("hero(HP:", herohp ,", attack:", heroatt ,")", lose," <-> monster(HP:",monsterhp,"), attack:",monsteratt,")",lose)
+  stack += 1
+  if(herohp<0):
+    judge = ("Monster Win!!!!")
+  else:
+    judge = ("Hero Win!!!!")
+print("---------------------------------------------------------")
+print("Total phase:",stack)
+print(judge)
