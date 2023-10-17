301ed7aa35d6b9544eb9bdabb7cab157eaa0f445
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-10-17 18:35:30 +0900

    Update prob2.py

--- chap6/prob2.py
+++ chap6/prob2.py
@@ -6,17 +6,23 @@ while choice != 0:
   print("0 - Quit")
   print("1 - List Scores")
   print("2 - Add a score")
-  
+
   choice = input("choice : ") 
 
   if choice == "0":
       print("bye")
+      break
   elif choice == "1":
-      for entry in scores:
-        score, name = entry
-        print(name, "\t", score)
+    print()
+    print("High scores\n")
+    print("NAME \t SCORE",)
+    for entry in scores:
+      name, score = entry
+      print(name, "\t", score)
+    print()
   elif choice == "2":
-      score = input(("What is the player's name?:"))
-      name = input(("what score did the player get?:"))
-      entry = (score, name)
+      name = input("What is the player's name?:")
+      score = int(input("what score did the player get?:"))
+      entry = (name, score)
       scores.append(entry)
+      scores.sort(key=lambda x: x[1], reverse= True)
