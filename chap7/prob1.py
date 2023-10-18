14bc643389c9b721817b3aa6e8fbbc98a078b067
moonkyeonghan <mkh2573@gmail.com>
2023-10-18 02:16:46 +0000

    chap7

---
+++ chap7/prob1.py
@@ -0,0 +1,25 @@
+print("Welcome to the gratest intellectual of all time: Tic-Tac-Toc.")
+print("This will be a shadown between your human brain and my silicon processor.\n")
+print("You will make your move known by entering a number, 0 - 8. The number will correspond to the board position as illustrated:\n")
+
+print("\t\t 0 | 1 | 2")
+print("\t\t-----------")
+print("\t\t 3 | 4 | 5")
+print("\t\t-----------")
+print("\t\t 6 | 7 | 8")
+print("Prepare yourself, human. The battle is about to begin.\n")
+
+first = input("Do you require the first move? (y/n):")
+
+if first == "y":
+  
+
+def background():
+  i = 0
+  row =0
+  col =0
+  for row in range(3):
+    for col in range(3):
+      print(i)
+      i += 1
+  background()
