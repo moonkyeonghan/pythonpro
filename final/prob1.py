16179e8abd6ad5cfeaaebcda635a3fbed9aa27fe
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-12-13 10:16:51 +0900

    Update prob1.py

--- final/prob1.py
+++ final/prob1.py
@@ -1,1 +1,13 @@
-ssssss
+def frequency_analytic(string):
+  count = {}
+  for c in string:
+    if c in count:
+      count[c] += 1
+    else:
+      count[c] = 1
+
+  for key, value in sorted(count.items()):
+    print(key, value)
+if __name__ == "__main__":
+  msg = input("input your message : ")
+  frequency_analytic(msg)
