ce8416e15bde057e72333977dd978671dc71daef
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-12-13 11:27:34 +0900

    Create blackjack.py

---
+++ final/blackjack.py
@@ -0,0 +1,237 @@
+(1)
+class Card:
+  Ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  Suit = ["S", "H", "D", "C"]
+
+  def __init__(self, rank, suit):
+      self.rank = rank
+      self.suit = suit
+
+  def __str__(self):
+      return self.rank + self.suit
+
+  def flip(self):
+    self.is_face_up = not self.is_face_up
+
+class Unprintable_Card(Card):
+
+  def __str__(self):
+      return "<unprintable>"
+
+
+class Hand:
+
+  def __init__(self):
+      self.cards = []
+
+  def __str__(self):
+      if self.cards:
+          reply = ""
+          for card in self.cards:
+              reply += str(card) + " "
+      else:
+          reply = "<empty>"
+
+      return reply
+
+  def clear(self):
+      self.cards = []
+
+  def add(self, card):
+      self.cards.append(card)
+
+  def give(self, card, other_hand):
+      self.cards.remove(card)
+      other_hand.add(card)
+
+class Deck(Hand):
+
+  def populate(self):
+      for suit in Card.Suit:
+          for rank in Card.Ranks:
+              self.add(Card(rank, suit))
+
+  def shuffle(self):
+      import random
+      random.shuffle(self.cards)
+
+  def deal(self, hands, per_hand=1):
+      for _ in range(per_hand):
+          for hand in hands:
+              if self.cards:
+                  top_card = self.cards[0]
+                  self.give(top_card, hand)
+              else:
+                  print("out of cards")
+
+
+class Positionable_Card(Card):
+
+  def __init__(self, rank, suit, face_up=True):
+      super().__init__(rank, suit)
+      self.is_face_up = face_up
+
+  def __str__(self):
+      if self.is_face_up:
+          reply = super().__str__()
+      else:
+          reply = "XX"
+      return reply
+
+  def flip(self):
+      self.is_face_up = not self.is_face_up
+
+def start_game():
+  print("Welcome to the world's simplest game!")
+  num_players = int(input("How many players? (2-5): "))
+
+  players = []
+  for i in range(num_players):
+      player_name = input(f"Enter player {i + 1}'s name: ")
+      players.append(player_name)
+
+  dealer = Positionable_Card(Card.Ranks[0], Card.Suit[0], True)
+  player_hands = [Hand() for _ in range(num_players)]
+
+  deck = Deck()
+  deck.populate()
+  deck.shuffle()
+
+  deck.deal(player_hands + [dealer], per_hand=2)
+  
+  if dealer.Positionable_Card():
+    dealer.flip()
+
+  print("Dealer's hand: ", dealer)
+
+    
+  for i, player_hand in enumerate(player_hands):
+      print(f"{players[i]}'s hand: {player_hand}")
+
+if __name__ == "__main__":
+  start_game()
+
+
+
+
+
+
+
+
+
+
+(2)
+class Card:
+  Ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  Suit = ["S", "H", "D", "C"]
+
+  def __init__(self, rank, suit):
+      self.rank = rank
+      self.suit = suit
+
+  def __str__(self):
+      return self.rank + self.suit
+
+  def flip(self):
+    self.is_face_up = not self.is_face_up
+
+class Unprintable_Card(Card):
+
+  def __str__(self):
+      return "<unprintable>"
+
+
+class Hand:
+
+  def __init__(self):
+      self.cards = []
+
+  def __str__(self):
+      if self.cards:
+          reply = ""
+          for card in self.cards:
+              reply += str(card) + " "
+      else:
+          reply = "<empty>"
+
+      return reply
+
+  def clear(self):
+      self.cards = []
+
+  def add(self, card):
+      self.cards.append(card)
+
+  def give(self, card, other_hand):
+      self.cards.remove(card)
+      other_hand.add(card)
+
+class Deck(Hand):
+
+  def populate(self):
+      for suit in Card.Suit:
+          for rank in Card.Ranks:
+              self.add(Card(rank, suit))
+
+  def shuffle(self):
+      import random
+      random.shuffle(self.cards)
+
+  def deal(self, hands, per_hand=1):
+      for _ in range(per_hand):
+          for hand in hands:
+              if self.cards:
+                  top_card = self.cards[0]
+                  self.give(top_card, hand)
+              else:
+                  print("out of cards")
+
+
+class Positionable_Card(Card):
+
+  def __init__(self, rank, suit, face_up=True):
+      super().__init__(rank, suit)
+      self.is_face_up = face_up
+
+  def __str__(self):
+      if self.is_face_up:
+          reply = super().__str__()
+      else:
+          reply = "XX"
+      return reply
+
+  def flip(self):
+      self.is_face_up = not self.is_face_up
+
+def start_game():
+  print("Welcome to the world's simplest game!")
+  num_players = int(input("How many players? (2-5): "))
+
+  players = []
+  for i in range(num_players):
+      player_name = input(f"Enter player {i + 1}'s name: ")
+      players.append(player_name)
+
+  dealer = Positionable_Card(Card.Ranks[0], Card.Suit[0], True)
+  player_hands = [Hand() for _ in range(num_players)]
+
+  deck = Deck()
+  deck.populate()
+  deck.shuffle()
+
+  deck.deal(player_hands + [dealer], per_hand=2)
+  
+  if dealer.Positionable_Card():
+    dealer.flip()
+
+  print("Dealer's hand: ", dealer)
+  
+  for i, player_hand in enumerate(player_hands):
+    for j, player_hand in enumerate(player_hands):
+      total[i] += player_hand.cards[j].rank
+    
+  for i, player_hand in enumerate(player_hands):
+     print(f"{players[i]}'s hand: {player_hand}", total[i])
+
+if __name__ == "__main__":
+  start_game()
