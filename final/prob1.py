41d9d0ca9c01cc467bb0f31148bb7d7db848e7af
moonkyeonghan <65268410+moonkyeonghan@users.noreply.github.com>
2023-12-13 10:26:59 +0900

    Update prob1.py

--- final/prob1.py
+++ final/prob1.py
@@ -7,7 +7,11 @@ def frequency_analytic(string):
       count[c] = 1
 
   for key, value in sorted(count.items()):
-    print(key, value)
+    if 96 <ord(key) < 123:
+      print(key, value)
+  for key, value in sorted(count.items()):
+    if 64 <ord(key) < 91:
+      print(key, value)
 if __name__ == "__main__":
   msg = input("input your message : ")
   frequency_analytic(msg)
